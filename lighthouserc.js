15338ae97dcd3848cab8a3af37f6988abc48b615
Kenny Ballard <kballard9@gmail.com>
2023-01-06 16:05:21 -0600

    adding public server info

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,9 @@ module.exports = {
         startServerCommand: 'npm run start',
       },
       upload: {
-        target: 'temporary-public-storage',
+        target: 'lhci',
+        serverBaseUrl: 'http://ec2-3-144-23-99.us-east-2.compute.amazonaws.com/',
+        token: '38114c83-8683-41ed-a3fe-f729685d7e2c',
       },
     },
   };
